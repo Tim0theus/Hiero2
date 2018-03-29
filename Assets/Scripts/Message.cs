e6a81aea26a47a71d9389c8c27ffa80bfa742365
Timotheus <timm.beckmann@tum.de>
2018-03-29 13:23:47 +0200

    repaired riddles

--- Assets/Scripts/Message.cs
+++ Assets/Scripts/Message.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Message : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+public class Message : Activatable, IPointerUpHandler, IPointerDownHandler {
 
     public Menu menu;
     public Text text;
     public TextAnchor anchor;
 
+    public bool startInactive = false;
+
     [TextArea(3,10)]
     public string message;
+
+    public override void Activate()
+    {
+        GetComponent<Renderer>().enabled = true;
+        GetComponent<Collider>().enabled = true;
+    }
+
+    public override void DeActivate()
+    {
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+    }
+
+    private void Start()
+    {
+        if (startInactive)
+            DeActivate();
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
