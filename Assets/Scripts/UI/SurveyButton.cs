0cee3c09ce3c8af4a5443b790eeade4a1c7ea1b2
Timotheus <timm.beckmann@tum.de>
2018-04-08 13:27:01 +0200

    new Survey

--- Assets/Scripts/UI/SurveyButton.cs
+++ Assets/Scripts/UI/SurveyButton.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class SurveyButton : MonoBehaviour {
 
     public void Click() {
-        Application.OpenURL("https://goo.gl/UBkU7f");
+        Application.OpenURL("https://goo.gl/forms/DF4Rorv5Vq7Jq8ln1");
     }
 }
